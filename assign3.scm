2fc4cf4db3ef09e4cef1d0ba9655a0bc1e5ccd40
Austin Schey <aschey13@gmail.com>
2016-11-14 20:02:11 -0500

    assignment 3

--- assign3.scm
+++ assign3.scm
@@ -81,3 +81,417 @@
       (else (+ (fib (- n 1)) (fib (- n 2))))
       )
     )
+  )
+
+(define (Stack) (_Stack (list) 0))
+
+(define (_Stack store size) this)
+
+(define (storeEmpty stack)
+  (= (length (stack 'store)) 0)
+  )
+
+(define (push stack val)
+  (_Stack (cons val (stack 'store)) (+ (stack 'size) 1))
+  )
+
+(define (pop stack)
+  (if (storeEmpty stack)
+    nil
+   (_Stack (cdr (stack 'store)) (- (stack 'size) 1))
+   )
+  )
+
+(define (speek stack)
+  (if (storeEmpty stack)
+    nil
+    (car (stack 'store))
+    )
+  )
+
+(define (ssize stack)
+  (stack 'size)
+  )
+
+; Implemented using two stacks
+; Inbox = stack values are enqueued on
+; Outbox = stack values are dequeued from
+; Values from the inbox are transferred to the outbox
+; if dequeue or qpeek is called
+(define (Queue) (_Queue (Stack) (Stack)))
+  
+(define (_Queue inbox outbox) this)
+
+(define (outboxEmpty queue)
+  (= (ssize (queue 'outbox)) 0)
+  )
+
+(define (inboxEmpty queue)
+  (= (ssize (queue 'inbox)) 0)
+  )
+
+(define (enqueue queue val)
+  (_Queue (push (queue 'inbox) val) (queue 'outbox))
+  )
+
+; Runs in O(length of inbox) if inbox is not empty
+; but overall amortized constant time because
+; this will only happen on the first time "dequeue" or "qpeek" is called
+; after an enqueue
+(define (makeOutbox in out)
+  (cond 
+    ((> (ssize in) 0)
+     (define nextVal (speek in))
+     (makeOutbox (pop in) (push out nextVal))
+     )
+    (else
+      (_Queue in out)
+      )
+    )
+  )
+
+(define (dequeue queue)
+  (cond 
+    ((outboxEmpty queue)
+     (if (inboxEmpty queue)
+       nil
+       (dequeue (makeOutbox (queue 'inbox) (queue 'outbox)))
+       )
+     )
+    (else (_Queue (queue 'inbox) (pop (queue 'outbox))))
+    )
+  )
+
+(define (qpeek queue)
+  (cond
+    ((outboxEmpty queue) 
+     (if (inboxEmpty queue)
+       nil
+       (qpeek (makeOutbox (queue 'inbox) (queue 'outbox)))
+       )
+     )
+    (else (speek (queue 'outbox)))
+    )
+  )
+
+(define (qsize queue)
+  (+ (ssize (queue 'inbox)) (ssize (queue 'outbox)))
+  )
+
+(define (avl)
+  (define (avlNode val)
+    (define left nil)
+    (define right nil)
+    (define height 0)
+    (define parent nil)
+    this
+    )
+
+  (define root nil)
+  (define treeSize 0)
+
+  (define (insert x)
+    (define node (avlNode x))
+    (cond
+      ((eq? root nil)
+       (set! root node)
+       )
+      (else
+        (_insert root node)
+        (setBalance node)
+        (insertionFixup node)
+        )
+      )
+    (set! treeSize (+ treeSize 1))
+    )
+
+  (define (size)
+    treeSize
+    )
+
+  (define (printTree)
+    (_printTree root)
+    )
+
+  (define (_insert curRoot node)
+    (cond
+      ((< (node 'val) (curRoot 'val))
+       (cond
+         ((eq? (curRoot 'left) nil)
+          (setLeftChild curRoot node)
+          )
+         (else
+           (_insert (curRoot 'left) node)
+           )
+         )
+       )
+      (else
+       (cond
+         ((eq? (curRoot 'right) nil)
+          (setRightChild curRoot node)
+          )
+         (else
+          (_insert (curRoot 'right) node)
+          )
+         )
+       )
+      )
+    )
+
+  (define (prune node)
+    (define parent (node 'parent))
+
+    (cond
+      ((eq? parent nil)
+       (set! root nil)
+       )
+      ((isLeftChild node)
+       (setLeftChild parent nil)
+       )
+      (else
+        (setRightChild parent nil)
+        )
+      )
+    )
+
+  (define (_printTree curRoot)
+    (cond
+      ((not (eq? (curRoot nil)))
+       (_printTree((curRoot 'left)))
+       (println (curRoot 'val))
+       (_printTree((curRoot 'right)))
+       )
+      )
+    )
+
+  (define (insertionFixup node)
+    (define break #f)
+    (while (and (not (eq? node root)) (not break))
+           (define parent (node 'parent))
+           (define parentFav (favorite parent))
+           (define sib (sibling node))
+           (cond
+             ((and (not (eq? sib nil)) (eq? parentFav sib))
+              (setBalance parent)
+              (set! break #t)
+              )
+             ((isBalanced parent)
+              (setBalance parent)
+              (set! node parent)
+              )
+             (else
+               (define fav (favorite node))
+               (cond
+                 ((not (eq? fav nil))
+                  (cond
+                    ((and (not (isLinear node fav)) (not (isLinear parent node)))
+                     (rotate node fav)
+                     (rotate parent fav)
+                     (setBalance node)
+                     (setBalance parent)
+                     (setBalance fav)
+                     )
+                    )
+                  )
+                 (else
+                   (rotate parent node)
+                   (setBalance parent)
+                   (setBalance node)
+                   )
+                 )
+               )
+             )
+           )
+    )
+
+  (define (find val)
+    (_find val root)
+    )
+
+  (define (_find val curRoot)
+    (cond
+      ((eq? curRoot nil)
+       #f
+       )
+      ((< val (curRoot 'val))
+       (_find val (curRoot 'left))
+       )
+      ((> val (curRoot 'val))
+       (_find val (curRoot 'right))
+       )
+      (else
+        #t
+        )
+      )
+    )
+
+  (define (setBalance node)
+    (define leftHeight 0)
+    (define rightHeight 0)
+    (if (not (eq? (node 'left) nil))
+      (set! leftHeight ((node 'left) 'height))
+      )
+    (if (not (eq? (node 'right) nil))
+      (set! rightHeight ((node 'right) 'height))
+      )
+    (define nodeHeight (node 'height))
+    (set 'height (+ (maxHeight leftHeight rightHeight) 1) node)
+    )
+
+  (define (rotate parent child)
+    (define grandparent (parent 'parent))
+
+    (cond
+      ((isLeftChild child)
+       (setLeftChild parent (child 'right))
+       (setRightChild child parent)
+       )
+      (else
+        (setRightChild parent (child 'left))
+        (setLeftChild child parent)
+        )
+      )
+
+    (cond
+      ((eq? grandparent nil)
+       (set! root child)
+       )
+      ((eq? (grandparent 'left) parent)
+       (setLeftChild grandparent child)
+       )
+      (else
+        (setRightChild grandparent child)
+        )
+      )
+    )
+
+  (define (maxHeight a b)
+    (if (> a b)
+      a
+      b
+      )
+    )
+
+  (define (sibling node)
+    (if (eq? node ((node 'parent) 'left))
+      ((node 'parent) 'right)
+      ((node 'parent) 'left)
+      )
+    )
+
+  (define (favorite parent)
+    (define balFact (balanceFactor parent))
+    (cond
+     ((= balFact 1)
+      (parent 'left)
+      )
+     ((= balFact -1)
+      (parent 'right)
+      )
+     (else
+       nil
+       )
+     )
+    )
+
+  (define (balanceFactor node)
+    (define leftHeight 0)
+    (define rightHeight 0)
+    (cond 
+      ((not (eq? (node 'right) nil))
+       (set! rightHeight ((node 'right) 'height))
+       )
+      )
+    (cond
+      ((not (eq? (node 'left) nil))
+       (set! leftHeight ((node 'left) 'height))
+       )
+      )
+    (- rightHeight leftHeight)
+    )
+
+  (define (isBalanced node)
+    (define balFact (balanceFactor node))
+    (and (< balFact 2) (> balFact -2))
+    )
+
+  (define (isLeftChild node)
+    (eq? node ((node 'parent) 'left))
+    )
+
+  (define (isRightChild node)
+    (eq? node ((node 'parent) 'right))
+    )
+
+  (define (setLeftChild parent child)
+    (set 'left child parent)
+    (cond
+      ((not (eq? child nil))
+       (set 'parent parent child)
+       )
+      )
+
+    ;(if (not (eq? child nil))
+    ;  (set 'parent parent child)
+    ;  )
+    )
+
+  (define (setRightChild parent child)
+    (set 'right child parent)
+    (cond
+      ((not (eq? child nil))
+       (set 'parent parent child)
+       )
+      )
+    ;(if (not (eq? child nil))
+    ;  (set 'parent parent child)
+    ;  )
+    )
+
+  (define (isLinear parent child)
+    (if (isLeftChild parent)
+      (isLeftChild child)
+      (isRightChild child)
+      )
+    )
+
+  (define (isLeaf node)
+    (and (null? (node 'left)) (null? (node 'right)))
+    )
+
+  (define (statistics)
+    (define queue (Queue))
+    (set! queue (enqueue queue root))
+    (while (> (qsize queue) 0)
+           (define current (qpeek queue))
+           (set! queue (dequeue queue))
+           ;(set! vals (append vals current))
+           (print (current 'val))
+           (print ":")
+           (print (* -1 (balanceFactor current)))
+           (print " ")
+           (if (not (eq? (current 'left) nil))
+             (set! queue (enqueue queue (current 'left)))
+             )
+           (if (not (eq? (current 'right) nil))
+             (set! queue (enqueue queue (current 'right)))
+             )
+           )
+    (println)
+    )
+
+  this
+  )
+
+(define (run3)
+  (define t (avl))
+  ((t 'insert) 3) 
+  ((t 'insert) 4)
+  ((t 'insert) 5)
+  ((t 'insert) 1)
+  ((t 'insert) 0)
+  ((t 'statistics))
+  (inspect ((t 'find) 7))
+  ;(inspect ((t 'size)))
+  )
+(run3)
